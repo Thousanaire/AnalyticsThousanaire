a50537f0d3932b94e3ead8a4e14ddd1431a2f7dc
Thousanaire <ThousanaireAi@outlook.com>
2026-03-01 00:45:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function calculateKPIs(rows) {
     const abandoned = sum(rows, "ABD #");
     const flowOuts = sum(rows, "FLOW OUTS");
 
-    const holds = # sum(rows, "# of HOLDS");
+    const holds = sum(rows, "# of HOLDS");
 
     const totalWait = sum(rows, "Wait time");
     const timeToAbandon = sum(rows, "TIME TO ABANDON");
@@ -382,4 +382,5 @@ loadData().then(rows => {
     renderKPIs(calculateKPIs(allRows));
     renderLOBChart(groupByProgram(allRows));
 });
+
 
